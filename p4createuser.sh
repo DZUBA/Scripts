2bdfd04b7c44862052aa81eaa760357622e304ca
Aleksey Dzyubenko <zzzdzuba@gmail.com>
2016-08-04 12:08:31 +0300

    Update p4createuser.sh

--- p4createuser.sh
+++ p4createuser.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 #List of users that need to be created. Every user typed in format "First Name" "Second Name", for example "Von Miller"
-list="~/p4users"
+list="$HOME/p4users"
 
 #Reading user list
-cat ~/p4users | while read newuser;
+cat $HOME/p4users | while read newuser;
 do
-	first=`echo $newuser | awk '{print $1}' | cut -c1 | tr '[:upper:]' '[:lower:]'`;
-	second=`echo $newuser | awk '{print $2}' | tr '[:upper:]' '[:lower:]'`;
+	first=$(echo $newuser | awk '{print $1}' | cut -c1 | tr '[:upper:]' '[:lower:]');
+	second=$(echo $newuser | awk '{print $2}' | tr '[:upper:]' '[:lower:]');
 	p4user="${first}_${second}"
-	echo $p4user >> ~/p4/p4logins
-	touch ~/p4/$p4user
-	p4userfile="~/p4/$p4user"
+	echo $p4user >> $HOME/p4/p4logins
+	touch $HOME/p4/$p4user
+	p4userfile="$HOME/p4/$p4user"
 echo "User: $p4user
 Email: $p4user@example.org
 FullName: $newuser" > $p4userfile;
